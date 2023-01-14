a184d2961604ce73b49de9f65de3290d29bf374b
Bobby Luly <bobbyluly@gmail.com>
2023-01-13 17:59:45 -0700

    Experimenting with runner and creating files

--- runner.rb
+++ runner.rb
@@ -3,10 +3,11 @@
 #ARGV == ["red.txt", "blue.txt"]
 # ARGV[0] == "red.txt"
 # ARGV[1] == "blue.txt"
-@user_input = ARGV
+# @user_input = ARGV
 
-message_file = File.open(ARVG[0], "r")
-new_file = File.new(ARVG[1], "w")
+message_file = File.open(ARGV[0], "r")
+new_file = File.new(ARGV[1], "w")
 
-new_file.write(message_file.read.downcase)
+new_file.write(message_file.read.upcase)
 new_file.close
+puts new_file
