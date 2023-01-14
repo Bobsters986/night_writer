52ab3c52d259eca510ffb20876dc9f847411b3d4
Bobby Luly <bobbyluly@gmail.com>
2023-01-14 15:58:18 -0700

    Change runner.rb -> night_writer.rb and move to lib folder

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,9 +1,3 @@
-# ruby colors.rb red.txt blue.txt
-
-#ARGV == ["red.txt", "blue.txt"]
-# ARGV[0] == "red.txt"
-# ARGV[1] == "blue.txt"
-# @user_input = ARGV
 
 message_file = File.open(ARGV[0], "r")
 new_file = File.new(ARGV[1], "w")
