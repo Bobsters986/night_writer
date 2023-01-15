d1a0b1d36f6aa4ac8a1dc8acfaf75aab49ffdfb4
Bobby Luly <bobbyluly@gmail.com>
2023-01-14 20:39:22 -0700

    Change some variable names to be more descriptive

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,10 +2,10 @@ message_file = ARGV[0]
 new_file = ARGV[1]
 
 m_file = File.open("./text_files/#{message_file}", "r")
-string = m_file.read
-n_file = File.new("./text_files/#{new_file}", "w")
+translated_file = File.new("./text_files/#{new_file}", "w")
 
-n_file.write(string.upcase)
-n_file.close
+text_string = m_file.read
+translated_file.write(text_string.upcase)
+# translated_file.close
 
-puts "Created '#{new_file}' containing #{string.length} characters"
+puts "Created '#{new_file}' containing #{text_string.length} characters"
