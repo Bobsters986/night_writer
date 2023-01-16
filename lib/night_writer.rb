a22a5e4cbcb9dc4bfa7b514b72ff83e68bcf715a
Bobby Luly <bobbyluly@gmail.com>
2023-01-16 14:08:37 -0700

    create instance of EngToBraille to call upon

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -2,13 +2,12 @@ require './lib/english_to_braille'
 
 message_file = ARGV[0]
 new_file = ARGV[1]
+translate = EngToBraille.new
 
 m_file = File.open("./text_files/#{message_file}", "r")
 translated_file = File.new("./text_files/#{new_file}", "w")
 
 text_string = m_file.read
-translated_file.write(EngToBraille.to_braille(text_string))
-# translated_file.write(text_string.upcase)
-# translated_file.close
+translated_file.write(translate.to_braille(text_string))
 
 puts "Created '#{new_file}' containing #{text_string.length} characters"
