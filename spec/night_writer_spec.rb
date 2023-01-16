efae21cdd859391ea672ab29cd1065a2cf8b5c6a
Bobby Luly <bobbyluly@gmail.com>
2023-01-16 14:10:10 -0700

    tied to create test for writing changes to a file. couldnt figure it out

--- spec/night_writer_spec.rb
+++ spec/night_writer_spec.rb
@@ -1,25 +1,25 @@
 # require './lib/night_writer'
 # require './text_files/test_message'
 
-describe 'night_writer' do
-  let(:argv) { ['test_message.txt', 'upcase.txt'] }
+RSpec.describe 'night_writer' do
+  let(:argv) { ['test_message.txt', 'braille_test.txt'] }
 
   it 'can open and read files' do
     test_message = argv[0]
     m_file = File.open("./text_files/#{test_message}", "r")
     string = m_file.read
-    expect(string).to eq("here is a message to be translated")
-    expect(string.length).to eq(34)
+    expect(string).to eq("message")
+    expect(string.length).to eq(7)
   end
 
   # it 'can write files' do
   #   test_message = argv[0]
   #   new_file = argv[1]
   #   m_file = File.open("./text_files/#{test_message}", "r")
-  #   string = m_file.read
   #   n_file = File.new("./text_files/#{new_file}", "w")
-  #   new_new = n_file.write(string.upcase)
-  #   read_new_file = File.open("./text_files/#{new_new}", "r")
+  #   string = m_file.read
+  #   n_file.write(string.upcase)
+  #   read_new_file = File.open("./text_files/#{new_file}", "r")
 
   #   translated_string = read_new_file.read
     
