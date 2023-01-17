36641193382684f6054c844751ca438ee8fd3429
Bobby Luly <bobbyluly@gmail.com>
2023-01-17 10:26:01 -0700

    Feat: break down main method again, add helper #to_braille_letters

--- lib/braille_to_english.rb
+++ lib/braille_to_english.rb
@@ -40,18 +40,27 @@ class BrailleToEng
 
   def to_character_rows(braille_string)
     braille_arr = braille_string.split("\n")
-    row_arr = braille_arr.map do |string|
+    row_array = braille_arr.map do |string|
       string.chars.each_slice(2).map do |char|
         char.join
       end
     end
-    row_arr
+    row_array
   end
-
-  def to_english_word(input)
-    row_arr = to_character_rows(input)
-    eng_letters = row_arr.transpose.map do |b_let|
+  
+  def to_braille_letters(braille_string)
+    row_array = to_character_rows(braille_string)
+    braille_letters = row_array.each_slice(3).flat_map do |letter|
+      letter.transpose
+    end
+    braille_letters
+  end
+  
+  def to_english_sentence(braille_string)
+    braille_letters = to_braille_letters(braille_string)
+    eng_letters = braille_letters.flat_map do |b_let|
       @inv_dictionary[b_let]
+      # require 'pry'; binding.pry
     end
     eng_letters.join
   end
